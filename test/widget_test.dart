6e2095024fc0e4d855699719348a18c9c6ddaf13
AhmedNaeim1 <ahmed.naiim@ngu.edu.eg>
2023-05-15 20:34:36 +0300

    updated widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,16 +7,19 @@ void main() {
     // Build the HomePage widget
     await tester.pumpWidget(const MaterialApp(home: HomePage()));
 
-    // Expect the initial background color to be the seed color
-    expect(tester.widget<Scaffold>(find.byType(Scaffold)).backgroundColor,
-        Colors.deepPurple);
+    // Get the initial background color
+    final initialColor =
+        tester.widget<Scaffold>(find.byType(Scaffold)).backgroundColor;
 
     // Tap on the widget
     await tester.tap(find.byType(GestureDetector));
     await tester.pump();
 
-    // Expect the background color to change
-    expect(tester.widget<Scaffold>(find.byType(Scaffold)).backgroundColor,
-        isNot(Colors.deepPurple));
+    // Get the updated background color
+    final updatedColor =
+        tester.widget<Scaffold>(find.byType(Scaffold)).backgroundColor;
+
+    // Expect the initial and updated colors to be different
+    expect(updatedColor, isNot(initialColor));
   });
 }
